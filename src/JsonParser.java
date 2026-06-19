5279c2912d5e978a8eb7757548a3cf805603dbd8
Alexander <aleksandr.lazarev.2002@gmail.com>
2026-06-20 01:17:24 +0300

    fix object field parsing

--- src/JsonParser.java
+++ src/JsonParser.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,11 +17,34 @@ public class JsonParser {
     }
 
     public static String findObjectField(String json, String field) throws Exception {
+        /*
         Pattern pattern = Pattern.compile("\"" + field + "\":\\{(.*)\\}");
         Matcher matcher = pattern.matcher(json);
         if (matcher.find()) {
             String entry = matcher.group();
             return entry.split(":")[1];
+        } else {
+            throw new Exception("Cannot find field " + field);
+        }
+        */
+
+        int startField = json.indexOf("\"" + field + "\":");
+        if (startField >= 0) {
+            startField += 3 + field.length();
+            Stack<Character> braceStack = new Stack<>();
+            int i = 0;
+            String ret = "";
+
+            do { 
+                char c = json.charAt(startField + i);
+                if (c == '{')
+                    braceStack.add('{');
+                if (c == '}')
+                    braceStack.pop();
+                ret += c;
+            } while (!braceStack.isEmpty());
+
+            return ret;
         } else {
             throw new Exception("Cannot find field " + field);
         }
